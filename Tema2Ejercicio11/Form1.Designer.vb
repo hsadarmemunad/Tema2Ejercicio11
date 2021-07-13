198bc3e1dc6f7f5ea4c506c72260176bdef0581a
Henry Adarme <hsadarmem@unadvirtual.edu.co>
2021-07-13 10:36:48 -0500

    Agregar archivos de proyecto.

---
+++ Tema2Ejercicio11/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxHeight = New System.Windows.Forms.TextBox()
+        Me.TextBoxWeight = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LabelIndex = New System.Windows.Forms.Label()
+        Me.LabelDescription = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBoxWeight)
+        Me.GroupBox1.Controls.Add(Me.TextBoxHeight)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(568, 202)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Altura y peso"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(23, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Altura"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(275, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 32)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Peso"
+        '
+        'TextBoxHeight
+        '
+        Me.TextBoxHeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxHeight.Location = New System.Drawing.Point(26, 109)
+        Me.TextBoxHeight.Name = "TextBoxHeight"
+        Me.TextBoxHeight.Size = New System.Drawing.Size(171, 38)
+        Me.TextBoxHeight.TabIndex = 2
+        '
+        'TextBoxWeight
+        '
+        Me.TextBoxWeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxWeight.Location = New System.Drawing.Point(278, 110)
+        Me.TextBoxWeight.Name = "TextBoxWeight"
+        Me.TextBoxWeight.Size = New System.Drawing.Size(171, 38)
+        Me.TextBoxWeight.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(203, 116)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 32)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "cm"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(455, 116)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 32)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Kg"
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(12, 265)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(568, 23)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Su índice de masa corporal es"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LabelIndex
+        '
+        Me.LabelIndex.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelIndex.Location = New System.Drawing.Point(12, 304)
+        Me.LabelIndex.Name = "LabelIndex"
+        Me.LabelIndex.Size = New System.Drawing.Size(568, 32)
+        Me.LabelIndex.TabIndex = 6
+        Me.LabelIndex.Text = "00"
+        Me.LabelIndex.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LabelDescription
+        '
+        Me.LabelDescription.AutoSize = True
+        Me.LabelDescription.Location = New System.Drawing.Point(9, 336)
+        Me.LabelDescription.Name = "LabelDescription"
+        Me.LabelDescription.Size = New System.Drawing.Size(199, 17)
+        Me.LabelDescription.TabIndex = 7
+        Me.LabelDescription.Text = "Su índice de masa corporal es"
+        Me.LabelDescription.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(592, 450)
+        Me.Controls.Add(Me.LabelDescription)
+        Me.Controls.Add(Me.LabelIndex)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculadora de índice de masa corporal"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBoxWeight As TextBox
+    Friend WithEvents TextBoxHeight As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents LabelIndex As Label
+    Friend WithEvents LabelDescription As Label
+End Class
---
+++ Tema2Ejercicio11/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub Calculate()
+        Dim height As Decimal
+        Dim weight As Decimal
+
+        If Decimal.TryParse(TextBoxHeight.Text, height) And Decimal.TryParse(TextBoxWeight.Text, weight) Then
+            Dim index As Decimal = 0
+
+            If height > 0 Then
+                height /= 100
+                index = weight / (height * height)
+            End If
+
+            LabelIndex.Text = Format(index, "0.0")
+            LabelDescription.Text = ""
+
+            If index < 18.5 Then
+                LabelDescription.Text = "Peso inferior al normal"
+            ElseIf index >= 18.5 And index < 25 Then
+                LabelDescription.Text = "Normal"
+            ElseIf index >= 25 And index < 30 Then
+                LabelDescription.Text = "Peso superior al normal"
+            ElseIf index >= 30 And index < 35 Then
+                LabelDescription.Text = "Obesidad tipo 2"
+            ElseIf index >= 35 And index < 40 Then
+                LabelDescription.Text = "Obesidad tipo 2"
+            End If
+        End If
+    End Sub
+
+    Private Sub TextBoxHeight_TextChanged(sender As Object, e As EventArgs) Handles TextBoxHeight.TextChanged
+        Calculate()
+    End Sub
+
+    Private Sub TextBoxWeight_TextChanged(sender As Object, e As EventArgs) Handles TextBoxWeight.TextChanged
+        Calculate()
+    End Sub
+End Class
